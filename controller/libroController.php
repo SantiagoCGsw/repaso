341225a6068fd4eae79ac9de4ed1b98f10c8c626
SantiagoCGsw <141855640+SantiagoCGsw@users.noreply.github.com>
2025-09-20 17:22:29 -0500

    Update libroController.php

--- controller/libroController.php
+++ controller/libroController.php
@@ -1,33 +1,27 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 include("../model/libroModel.php");
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST["accion"])) {
-        if ($_POST["accion"] === "insertar") {
-            insertarLibro(
-                $_POST["titulo"],
-                $_POST["autor"],
-                $_POST["fecha_lectura"],
-                $_POST["terminado"],
-                $_POST["ultimo_capitulo"]
-            );
-        } elseif ($_POST["accion"] === "editar") {
-            actualizarLibro(
-                $_POST["id"],
-                $_POST["titulo"],
-                $_POST["autor"],
-                $_POST["fecha_lectura"],
-                $_POST["terminado"],
-                $_POST["ultimo_capitulo"]
-            );
-        }
-        header("Location: ../view/lista.php");
-        exit();
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["accion"])) {
+    if ($_POST["accion"] === "insertar") {
+        insertarLibro(
+            $_POST["titulo"],
+            $_POST["autor"],
+            $_POST["fecha_lectura"] ?? null,
+            $_POST["terminado"] ?? 0,
+            $_POST["ultimo_capitulo"] ?? null
+        );
+    } elseif ($_POST["accion"] === "editar") {
+        actualizarLibro(
+            $_POST["id"],
+            $_POST["titulo"],
+            $_POST["autor"],
+            $_POST["fecha_lectura"] ?? null,
+            $_POST["terminado"] ?? 0,
+            $_POST["ultimo_capitulo"] ?? null
+        );
     }
+    header("Location: ../view/lista.php");
+    exit();
 }
 
 if (isset($_GET["eliminar"])) {
