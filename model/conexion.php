7a8bbd0f53f39cf688af83fb65bf53f551531a86
SantiagoCGsw <141855640+SantiagoCGsw@users.noreply.github.com>
2025-09-20 15:40:32 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,7 +1,12 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "Swolt", "biblioteca", 3307);
+$host = "sql205.infinityfree.com";   // MySQL Hostname
+$user = "if0_39988467";              // MySQL Username
+$pass = "XA8LPv736JNX";              // MySQL Password
+$db   = "if0_39988467_XXX";          // Reemplaza XXX por el nombre exacto de tu BD
+
+$conexion = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conexion) {
-    die("Error en la conexión: " . mysqli_connect_error());
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
