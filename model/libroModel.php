9fdad28cc5c4bf98da0340b8bf7a494b94e2f7c0
SantiagoCGsw <141855640+SantiagoCGsw@users.noreply.github.com>
2025-09-20 16:09:59 -0500

    Update libroModel.php

--- model/libroModel.php
+++ model/libroModel.php
@@ -1,9 +1,17 @@
 <?php
 include("conexion.php");
 
+// Mostrar errores (temporal)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 function obtenerLibros() {
     global $conexion;
     $resultado = mysqli_query($conexion, "SELECT * FROM libros");
+    if (!$resultado) {
+        die("Error al obtener libros: " . mysqli_error($conexion));
+    }
     return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 }
 
@@ -12,27 +20,45 @@ function insertarLibro($titulo, $autor) {
     $titulo = mysqli_real_escape_string($conexion, $titulo);
     $autor = mysqli_real_escape_string($conexion, $autor);
     $query = "INSERT INTO libros (titulo, autor) VALUES ('$titulo','$autor')";
-    return mysqli_query($conexion, $query);
+    $resultado = mysqli_query($conexion, $query);
+    if (!$resultado) {
+        die("Error al insertar libro: " . mysqli_error($conexion));
+    }
+    return $resultado;
 }
 
 function obtenerLibro($id) {
     global $conexion;
+    $id = intval($id); // asegurar que sea número
     $query = "SELECT * FROM libros WHERE id=$id";
     $resultado = mysqli_query($conexion, $query);
+    if (!$resultado) {
+        die("Error al obtener libro: " . mysqli_error($conexion));
+    }
     return mysqli_fetch_assoc($resultado);
 }
 
 function actualizarLibro($id, $titulo, $autor) {
     global $conexion;
+    $id = intval($id);
     $titulo = mysqli_real_escape_string($conexion, $titulo);
     $autor = mysqli_real_escape_string($conexion, $autor);
     $query = "UPDATE libros SET titulo='$titulo', autor='$autor' WHERE id=$id";
-    return mysqli_query($conexion, $query);
+    $resultado = mysqli_query($conexion, $query);
+    if (!$resultado) {
+        die("Error al actualizar libro: " . mysqli_error($conexion));
+    }
+    return $resultado;
 }
 
 function eliminarLibro($id) {
     global $conexion;
+    $id = intval($id);
     $query = "DELETE FROM libros WHERE id=$id";
-    return mysqli_query($conexion, $query);
+    $resultado = mysqli_query($conexion, $query);
+    if (!$resultado) {
+        die("Error al eliminar libro: " . mysqli_error($conexion));
+    }
+    return $resultado;
 }
 ?>
