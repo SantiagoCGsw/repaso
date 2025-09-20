97ff4bcfa0b5adee2e7feeca4b6a7f4e9b459454
SantiagoCGsw <141855640+SantiagoCGsw@users.noreply.github.com>
2025-09-20 16:25:31 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,14 +1,12 @@
 <?php
-$host = "sql205.infinityfree.com";   // Host de InfinityFree
-$user = "if0_39988467";              // Usuario MySQL
-$pass = "XA8LPv736JNX";              // Contraseña MySQL
-$db   = "if0_39988467_libros";       // Nombre exacto de la BD
+$host = "sql205.infinityfree.com";
+$user = "if0_39988467";
+$pass = "XA8LPv736JNX";
+$db   = "if0_39988467_libros";
 
 $conexion = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conexion) {
     die("❌ Error de conexión: " . mysqli_connect_error());
-} else {
-    // echo "✅ Conexión exitosa"; // puedes descomentar para probar
 }
 ?>
